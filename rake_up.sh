576dd83a51e2dcff02633eba72fc429a6bb629a1
msl <msl@mercury.local>
2014-06-05 12:11:58 -0700

    Auto-update

--- rake_up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-RAKEFILE_REPO=robins-rakefile
-
-git submodule init
-git submodule add git@bitbucket.org:robinrob/rakefile.git $RAKEFILE_REPO
-ln -s $RAKEFILE_REPO/Rakefile ./
-rake save
