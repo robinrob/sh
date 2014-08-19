28d42b06954c0257ac9cdecb2223c65ede0f9512
Robin Smith <robin.smith@cloudreach.co.uk>
2014-08-19 02:34:53 -0700

    Auto-update

--- args.sh
+++ args.sh
@@ -11,6 +11,9 @@ echo ${args[0]} ${args[1]} ${args[2]} ' -> args=("$@"); echo ${args[0]} ${args[1
 #use $@ to print out all arguments at once
 echo $@ ' -> echo $@'
 
+#or $*
+echo $* ' -> echo $*'
+
 # use $# variable to print out
 # number of arguments passed to the bash script
 echo Number of arguments passed: $# ' -> echo Number of arguments passed: $#' 
---
+++ join.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+IFS="$1"; shift; echo "$*";
