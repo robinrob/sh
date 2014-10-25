186fec3234479b78d482ed4842b530acc21a3f57
Robin Smith <robin@mrrobinsmith.com>
2014-10-25 01:41:13 -0700

    Auto-update.

---
+++ practice/and.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+if [ " " -a " " ]
+then
+  echo "True!"
+else
+  echo "False."
+fi
---
+++ practice/or.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+if [ "" -o " " ]
+then
+  echo "True!"
+else
+  echo "False."
+fi
