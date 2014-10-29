e0d624605be80679370bb493e069acf2c10edf6d
msl <robin@mrrobinsmith.com>
2014-10-29 00:55:09 -0700

    Auto-update.

---
+++ practice/sed/replace.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+source $SHCOLORS_PATH
+source $SHLOG_PATH
+
+
+log "echo 'robin' | sed 's/o/O/g'"
---
+++ templates/practice.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+source $SHCOLORS_PATH
+source $SHLOG_PATH
+
