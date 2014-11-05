ae12a9693dcbf9c80dc8098c40a28cff4f34f8e1
Robin Simth <robin@mrrobinsmith.com>
2014-11-04 20:41:17 -0800

    Auto-update.

--- practice/encrypt.sh
+++ practice/encrypt.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env sh
 
-echo "String to Encrypt" | openssl rsautl -pubin -inkey id_rsa.pub -encrypt -pkcs | openssl enc -base64
+source $SHLOG_PATH
 
+
+STR=$1
+
+# Use this command to generate a PEM public key:
+# openssl rsa -in ~/.ssh/id_rsa -pubout > ~/.ssh/id_rsa.pub.pem
+
+green "Encrypting '$STR' ..."
+rm -f encrypted.txt && echo $STR | openssl rsautl -encrypt -pubin -inkey ~/.ssh/id_rsa.pub.pem > encrypted.txt
+yellow `cat encrypted.txt`
+
+green "Decrypting $STR: $(yellow) `cat encrypted.txt | openssl rsautl -decrypt -inkey ~/.ssh/id_rsa`"
