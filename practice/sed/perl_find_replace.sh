255f0770bf51e7a07bed9c83202bd0e1320f1f9c
Robin Smith <robin@mrrobinsmith.com>
2015-05-17 17:29:50 +0100

    Auto-update.

--- practice/sed/perl_find_replace.sh
+++ practice/sed/perl_find_replace.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+# -p flag applies the entire Perl program to each line of the file
+# -e flag means that the Perl program follows
 # -i flag can be used as with `sed` to write back to the file
 perl -p -e 's/andrew/programming/g' name.txt
